e151e12015c2d4dafd14de91c30e2e4ebfe39409
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2020-07-09 18:26:12 -0500

    Most of MVP done, just the last two files left!

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -5,15 +5,30 @@ at how to read and write to files here:
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
+
+
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
 
+with open('./foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+    f.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open('./bar.txt', "w") as f2:
+    f2.write("One line\nTwo lines\nThree lines")
+    f2.close()
+
+with open('./bar.txt', 'r') as f2:
+    read_data = f2.read()
+    print(read_data)
+    f2.close()
